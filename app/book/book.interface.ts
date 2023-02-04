1d64d7d425ce09fdd8d4e660d028736573ebbc01
cesek-navisys <79996604+cesek-navisys@users.noreply.github.com>
2023-02-04 11:14:12 +0100

    todo: Update book.interface.ts

--- app/book/book.interface.ts
+++ app/book/book.interface.ts
@@ -1,6 +1,8 @@
 import { IAuthor } from "../author/author.interface";
 import { IPublisher } from "../publisher/publisher.interface";
 
+// TODO: přidat propery code (string), které bude obaahovat unikátní uuid
+
 export interface IBook {
   name: string;
   author: IAuthor;
