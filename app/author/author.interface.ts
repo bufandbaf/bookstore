7d04b0994452688926d0cb16545d3c31423fd956
Tomas Cesek <cesek.tomas@navisys.cz>
2023-02-06 14:36:58 +0100

    refactor: author code

--- app/author/author.interface.ts
+++ app/author/author.interface.ts
@@ -1,6 +1,7 @@
 // TODO: přidat do IAuthor unikátní code a rozdělit interfaces na IAuthorView a IAuthorCreate
 
 export interface IAuthorView {
+  code: string;
   firstName: string;
   lastName: string;
 }
