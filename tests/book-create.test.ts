b831d00c03d989fddc2f1dfd648e043ede87bba9
Tomas Cesek <cesek.tomas@navisys.cz>
2023-02-07 12:59:34 +0100

    refactor: book create to review

--- tests/book-create.test.ts
+++ tests/book-create.test.ts
@@ -1,7 +1,26 @@
-// Author creation
+import { IAuthorCreate } from "../app/author/author.interface";
+import { IBookCreate } from "../app/book/book.interface";
+import { IPublisher } from "../app/publisher/publisher.interface";
 
-// Arrange
+/**
+const testNewBook = () => {
+  const newAuthor: IAuthorCreate = {
+    firstName: "John",
+    lastName: "Doe",
+  };
+  const newPublisher: IPublisher = { name: "Tatravoid" };
 
-// Act
 
-// Assert
+ * const newBook: IBookCreate = {
+    name: "Test Book",
+    author: newAuthor,
+    publisher: newPublisher,
+  };
+  expect(newBook.name).toEqual("Test Book");
+  expect(newBook.author.firstName).toEqual("John");
+  expect(newBook.author.lastName).toEqual("Doe");
+  expect(newBook.publisher.name).toEqual("Tatravoid");
+};
+
+testNewBook();
+*/
