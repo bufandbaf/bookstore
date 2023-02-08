9d1660b44302657472a67cc646b7edb1319d5e0f
Michal Cesek <cesek@navisys.cz>
2023-02-08 13:27:16 +0100

    feat: jest-like describe

--- tests/book-create.test.ts
+++ tests/book-create.test.ts
@@ -3,17 +3,16 @@ import {AuthorService} from '../app/author/author.service';
 import { IBookCreate } from '../app/book/book.interface';
 import { BookService } from '../app/book/book.service';
 import { Genre } from '../app/book/genre.enum';
-import {expect, expectObjectContains} from './utils/jest-like-expect';
+import {describe, expect, expectObjectContains} from './utils/jest-like-expect';
 
 // Service Injection
 const authorService = AuthorService.getInstance();
 const bookService = BookService.getInstance();
 
 // Arrange
-const expectedAuthor: Pick<IAuthorView, 'firstName' | 'lastName'> & {nesmysl: string} = {
+const expectedAuthor: Pick<IAuthorView, 'firstName' | 'lastName'> = {
   firstName: "John",
   lastName: "Doe",
-  nesmysl: 'abc',
 }
 const expectedBook: Pick<IBookCreate, 'name' | 'genre' | 'publisher'> = {
   name: "Seven",
@@ -34,9 +33,14 @@ bookService.create({
 const book = bookService.findByName(expectedBook.name, author.code);
 
 // Assert
-console.clear();
-let foundAnyError: boolean = false;
-if (!expect('book name', book.name, expectedBook.name)) foundAnyError = true;
-if (!expectObjectContains(book?.author, expectedAuthor)) foundAnyError = true;
-if (!foundAnyError)
-  console.log('Test for book creation was successfully provided.');
+describe('Testing books', () => {
+
+  describe('Book name', () => {
+    expect(book.name, expectedBook.name);
+  });
+
+  describe('Book object', () => {
+    expectObjectContains(book?.author, expectedAuthor, {showSummary: true});
+  })
+
+});
--- tests/utils/jest-like-expect.ts
+++ tests/utils/jest-like-expect.ts
@@ -1,3 +1,6 @@
+let foundAnyError: boolean = false;
+let firstRun = true;
+
 /**
  * 
  * @param valueName display name of entity column or any other name
@@ -5,10 +8,14 @@
  * @param expected expected value
  * @returns true if expect is equal to received
  */
-export function expect(valueName: string, received: any, expected: any) {
+export function expect(received: any, expected: any) {
     const success = expected === received;
-    if (!success)
-        console.error('ERROR!!!', `Expected "${expected}" in ${valueName} but received "${received}"!`);
+    if (!success) {
+        console.error('Received:', received);
+        console.error('Expected:', expected);
+        console.log();
+    }
+    if (!success) foundAnyError = true;
     return success;
 }
 
@@ -18,10 +25,12 @@ export function expect(valueName: string, received: any, expected: any) {
  * 
  * @param received received (real) object with values
  * @param expected expected object values
+ * @param options additional options
  * @returns true if expect is equal to received
  */
-export function expectObjectContains(received: Record<string, any> | undefined, expected: Record<string, any> | undefined) {
+export function expectObjectContains(received: Record<string, any> | undefined, expected: Record<string, any> | undefined, options?: {showSummary?: boolean}) {
     let success = true;
+    console.info('Testing object...');
     if (typeof received !== 'object') {
         console.error('Received value is not an object.')
         return false;
@@ -33,23 +42,42 @@ export function expectObjectContains(received: Record<string, any> | undefined, 
     for (const entry of Object.entries(expected)) {
         if (!received.hasOwnProperty(entry[0])) {
             console.error(`Received object does not have "${entry[0]}" property.`)
-            console.log('');
+            console.log();
             success = false;
         }
 
         if (received[entry[0]] !== expected[entry[0]]) {
-            console.error('Property name:', entry[0]);
-            console.error('Received:', received[entry[0]]);
-            console.error('Expected:', expected[entry[0]]);
-            console.log('');
+            console.error('> Property name:', entry[0]);
+            console.error('> Expected:', expected[entry[0]]);
+            console.error('> Received:', received[entry[0]]);
+            console.log();
             success = false;
         }
     }
 
-    if (!success) {
-        console.error('Object received:', received);
-        console.error('Object expected:', expected);
-        console.log('');
+    if (!success && options?.showSummary) {
+        console.error('Summary:')
+        console.error('> expected:', expected);
+        console.error('> received:', received);
+        console.log();
     }
+    if (!success) foundAnyError = true;
     return success;
+}
+
+export function describe(name: string, callback: Function) {
+    foundAnyError = false;
+    if (firstRun) {
+        console.clear();
+        console.info(`*** ${name} ***`);
+        firstRun = false;
+    }
+    console.log();
+    callback();
+    if (foundAnyError){
+        console.error(`RESULT: ${name} failed!`)
+    } else {
+        console.log(`RESULT: ${name} succeeded.`);
+    }
+    console.log();
 }
