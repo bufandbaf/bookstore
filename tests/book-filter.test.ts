48830036c372af87df781468fac92a4f113b682c
Tomas Cesek <cesek.tomas@navisys.cz>
2023-02-15 10:20:11 +0100

    refactor: book filter

--- tests/book-filter.test.ts
+++ tests/book-filter.test.ts
@@ -13,26 +13,31 @@ try {
     // Arrange
     const expectedBook = bookCreateSamples.animalFarm.name;
     const expectedAuthor = authorCreateSamples.georgeOrwell;
-    
+
     // Act
     const author = authorService.create(expectedAuthor);
-    const filteredBook = bookService.create({
+    const book1 = bookService.create({
         name: expectedBook,
         authorCode: author.code,
         genre: expectedBook as Genre,
     });
+    const book2 = bookService.create({
+        name: '1984',
+        authorCode: author.code,
+        genre: 'SCIENCE FICTION' as Genre,
+    });
 
-    const filteredBooks = bookService.filterByName('');
+    const filteredBooks = bookService.filterByName(expectedBook);
 
     // Assert
     describe('Testing book filtering by name', () => {
         describe('Check if the correct book was filtered.', () => {
-            expect(filteredBooks, expectedBook);
+            expect(filteredBooks.length, 1);
+            expect(filteredBooks[0].name, expectedBook);
         });
 
         describe('Filtered book details', () => {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            expectObjectContains(filteredBook, expectedBook as any, {
+            expectObjectContains(filteredBooks[0], book1, {
                 showSummary: true,
             });
         });
